629502299bdb647c7c20eabf7571a840b60fe7c5
sean <morriss@me.com>
2018-12-12 14:27:00 +0000

    finally figured out how xfail should work

--- tests/use_cases/test_uploader_use_case.py
+++ tests/use_cases/test_uploader_use_case.py
@@ -6,7 +6,7 @@ def test_smoke_test():
     assert True
 
 
-@pytest.xfail
+@pytest.mark.xfail
 def test_list_all_entry_revision_content(fs_data_storage, test_entry_uid):
     data_storage = fs_data_storage(test_entry_uid)
     all_resources = data_storage.ls()
