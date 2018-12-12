d62fb3d6f5cf7f72b46320b233e94cce58a3722b
sean <morriss@me.com>
2018-12-12 14:22:06 +0000

    rename use case tests directory

--- tests/use_cases/uploader_use_case_tests.py
+++ tests/use_cases/uploader_use_case_tests.py
@@ -2,6 +2,10 @@ import pytest
 from .. import fs_data_storage, test_entry_uid
 
 
+def test_smoke_test():
+    assert True
+
+
 @pytest.xfail
 def test_list_all_entry_revision_content(fs_data_storage, test_entry_uid):
     data_storage = fs_data_storage(test_entry_uid)
